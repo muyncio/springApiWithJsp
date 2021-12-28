4bde78ae412a80a669b2921b23dc81131768eb18
muyncio <muynek@gmail.com>
2021-12-28 13:52:41 +0100

    adding queries, needs mapping

--- src/main/java/com/example/api/controller/TestTableController.java
+++ src/main/java/com/example/api/controller/TestTableController.java
@@ -23,15 +23,15 @@ public class TestTableController {
     }
 
     @GetMapping({"/table"})
-    public String listTestTables( @RequestParam(value = "code", defaultValue = "bigos")String code, Model model){
+    public String listTestTables( @RequestParam(value = "code", defaultValue = "brak")String code, Model model){
         Session session = HibernateFactory.getInstance().getSessionFactory().getCurrentSession();
         Transaction tx = null;
         List<String> htmlList = new ArrayList<>();
 
         try {
             tx = session.beginTransaction();
-            List employees = session.createQuery("FROM TestTable").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+            List values = session.createQuery("FROM TestTable").list();
+            for (Iterator iterator = values.iterator(); iterator.hasNext();){
                 TestTable testTable = (TestTable) iterator.next();
                 htmlList.add("<tr> " +
                         "<td>" + testTable.getId() + "</td> " +
@@ -56,15 +56,19 @@ public class TestTableController {
         return "table";
     }
     @GetMapping({"/tablewithdifferent"})
-    public String listTestTablesWitchTheSameVariables( @RequestParam(value = "code", defaultValue = "bigos")String code, Model model){
+    public String listTestTablesWitchTheSameVariables( @RequestParam(value = "code", defaultValue = "brak")String code, Model model){
         Session session = HibernateFactory.getInstance().getSessionFactory().getCurrentSession();
         Transaction tx = null;
         List<String> htmlList = new ArrayList<>();
 
         try {
             tx = session.beginTransaction();
-            List employees = session.createQuery("FROM TestTable ").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+            List values = session.createQuery("select t2.id, t2.kolumna1, t2.kolumna2, t2.kolumna3, t2.kolumna4 from TestTable t1\n" +
+                    "join TestTable t2 on t2.kolumna1 not in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "and t2.kolumna2 not in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "and t2.kolumna3 not in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "where t1.id = 1").list();
+            for (Iterator iterator = values.iterator(); iterator.hasNext();){
                 TestTable testTable = (TestTable) iterator.next();
                 htmlList.add("<tr> " +
                         "<td>" + testTable.getId() + "</td> " +
@@ -89,15 +93,19 @@ public class TestTableController {
         return "tablewithdifferent";
     }
     @GetMapping({"/tablewiththesame"})
-    public String listTestTablesWithTheSameVariables( @RequestParam(value = "code", defaultValue = "bigos")String code, Model model){
+    public String listTestTablesWithTheSameVariables( @RequestParam(value = "code", defaultValue = "brak")String code, Model model){
         Session session = HibernateFactory.getInstance().getSessionFactory().getCurrentSession();
         Transaction tx = null;
         List<String> htmlList = new ArrayList<>();
 
         try {
             tx = session.beginTransaction();
-            List employees = session.createQuery("FROM TestTable").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+            List values = session.createQuery("select t2.id, t2.kolumna1, t2.kolumna2, t2.kolumna3, t2.kolumna4 from TestTable t1\n" +
+                    "join TestTable t2 on t2.kolumna1 in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "and t2.kolumna2 in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "and t2.kolumna3 in (t1.kolumna1, t1.kolumna2, t1.kolumna3) \n" +
+                    "where t1.id = 1").list();
+            for (Iterator iterator = values.iterator(); iterator.hasNext();){
                 TestTable testTable = (TestTable) iterator.next();
                 htmlList.add("<tr> " +
                         "<td>" + testTable.getId() + "</td> " +
